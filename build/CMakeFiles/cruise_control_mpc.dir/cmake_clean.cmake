ba1d4b74f4c17beda0bf06dacf46b282445d2197
khkhh123 <khkhh@g.skku.edu>
2026-02-06 21:49:21 +0900

    CasADi 설치 (빌드, 설치 파일은 제외)

---
+++ build/CMakeFiles/cruise_control_mpc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cruise_control_mpc.dir/src/main.cpp.o"
+  "CMakeFiles/cruise_control_mpc.dir/src/main.cpp.o.d"
+  "cruise_control_mpc"
+  "cruise_control_mpc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cruise_control_mpc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
